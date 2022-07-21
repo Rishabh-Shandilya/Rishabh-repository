37ee6beb73bcdaa0b5060de14a5e4ec9a8c4e6c6
Rishabh-Shandilya <Rishabh.shandilya@resilientitservices.com>
2022-07-21 06:08:33 +0530

    Rishabh|| new prog

---
+++ Rishabh_Repository_Assignments/src/com/Assignment/ifSwitch/Q10_CharacterCase.java
@@ -0,0 +1,18 @@
+package com.Assignment.ifSwitch;
+
+import java.util.Scanner;
+
+public class Q10_CharacterCase {
+	public static void main(String[] args) {
+		char ch;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a character to check whether it is uppercase or lowercase");
+		ch = input.next().charAt(0);
+		if (ch>='a'&&ch<='z') {
+			System.out.println(ch+" is a lowercase");
+		}//end if
+		else if (ch>='A'&&ch<='Z') {
+			System.out.println(ch+" is an uppercase");
+		}//end else if
+	}// end main
+}// end class
